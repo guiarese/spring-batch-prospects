39d87c0688381d1522f6cb182c421dd4fa876090
guiarese <guilhermearesende1@gmail.com>
2021-01-12 23:44:57 -0300

    First Commit

---
+++ src/main/java/com/guiarese/springbatchprospects/SpringBatchProspectsApplication.java
@@ -0,0 +1,13 @@
+package com.guiarese.springbatchprospects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchProspectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchProspectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guiarese/springbatchprospects/SpringBatchProspectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiarese.springbatchprospects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchProspectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
